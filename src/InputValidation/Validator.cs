80b2f78f289af2f48d2c347a5e8c17aaba2b58c4
KZRM <kallezrm@gmail.com>
2024-04-23 16:04:43 +0200

    Validator and Pizza test

--- src/InputValidation/Validator.cs
+++ src/InputValidation/Validator.cs
@@ -4,7 +4,43 @@ public class Validator : IValidator
 {
   public bool IsValidEmail(string email)
   {
-    // TODO - replace the below exception with your own implementation
-    throw new NotImplementedException();
+        // TODO - replace the below exception with your own implementation
+
+        bool isValidEmail = true;
+        string failCase = "Email invalid: ";
+
+        //Check if it contains "@"
+        if (!email.Contains("@"))
+        {
+            isValidEmail = false;
+            failCase = failCase + "   needs a @";
+        }
+
+
+        //Check if there are any letters before @
+        int letterIndexOfEmail = email.IndexOf("@");
+        if
+            (letterIndexOfEmail == 0)
+        {
+            isValidEmail = false;
+            failCase = failCase + "   needs text before @";
+        }
+
+
+        //Check if email ends with ".com"
+        if (!email.EndsWith(".com"))
+        {
+            isValidEmail = false;
+            failCase = failCase + "   needs .com at end of mail";
+        }
+
+        if (isValidEmail)
+        {
+            failCase = "mail is valid!";
+        }
+
+        Console.WriteLine(failCase);
+        return isValidEmail;
+
   }
 }
--- src/MarioPizza/Menu.cs
+++ src/MarioPizza/Menu.cs
@@ -4,10 +4,68 @@ namespace MarioPizza;
 
 public class Menu : IMenu
 {
-  public ICollection<IPizza> AllPizzas { get; set; }
-  
-  public ICollection<string> FindPizza(IList<string> mustHaveIngredients, IList<string> wontHaveIngredients)
-  {
-    throw new NotImplementedException();
-  }
+    public ICollection<IPizza> AllPizzas { get; set; }
+
+    public ICollection<string> FindPizza(IList<string> mustHaveIngredients, IList<string> wontHaveIngredients)
+    {
+        //New list of strings containing pizzas matching the requried ingredients.
+        var matchingPizzas = new List<string>();
+
+
+        string validPizzaNames = "Pizza's you would like: ";
+
+        foreach (IPizza pizza in AllPizzas)
+        {
+
+            bool validPizza = true;
+
+            //Checks if any of the Pizzas contains the mustHaveIngredients in ingredients list.
+            foreach (string mustHaveIngredient in mustHaveIngredients)
+            {
+                if (!pizza.Ingredients.Contains(mustHaveIngredient))
+                {
+                    validPizza = false;
+                    break;
+                }
+            }
+
+            foreach (string wontHaveIngredient in wontHaveIngredients)
+            {
+                if (pizza.Ingredients.Contains(wontHaveIngredient))
+                {
+                    validPizza = false;
+                    break;
+                }
+            }
+
+
+
+            if (validPizza)
+            {
+                matchingPizzas.Add(pizza.Name);
+                validPizzaNames = validPizzaNames + pizza.Name + ", ";
+            }
+
+
+
+
+        }
+
+        if (matchingPizzas.Count > 0)
+        {
+            validPizzaNames = validPizzaNames.Substring(0, validPizzaNames.Length - 2) + "!";
+        }
+        else
+        {
+            validPizzaNames = "...Found no pizzas for you, Mr. picky!";
+        }
+
+
+        Console.WriteLine(validPizzaNames);
+        //Returns the pizza matching the customer demands.
+        return matchingPizzas;
+
+
+
+    }
 }
